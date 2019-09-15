4cacf041a30c809c04072ddc92182dbcf959e81b
Alex Williams <williams_alex@protonmail.com>
2019-09-14 21:38:07 -0400

    New functioning polynomial generator
    Working build, creates polynomials to specifications and tests them. Changed balance of generation from createPolynomial to fixpoly and integrated with Singular

--- math.py
+++ math.py
@@ -1,7 +1,6 @@
 # check to make sure all libraries are necessary
 import random
 import decimal
-import thread
 import time
 from decimal import *
 from random import uniform
@@ -24,85 +23,87 @@ class poly():
 # class that holds the data for a single term in a polynomial
 
 class term():
-    def __init__(self, variable, constant, exponent):
-        self.v = variable
+    def __init__(self, variables, constant, exponent):
+        self.v = variables
         self.c = constant
         self.e = exponent
-        self.f = int(random.uniform(1,variable))
+        self.f = int(random.uniform(1,variables))
 
-# class that creates the names of the variables
+# function that creates the names of the variables
 # needs to be expanded upon
 
-def createVarNames(poly):
+def createVarNames(numvars):
     varNames=[]
-    if(poly.v<27):
-        for x in range(poly.v):
+    if(numvars<27):
+        for x in range(numvars):
             varNames.append(chr(x+65))
     else:
         return "too many variables!"
     return varNames
 
+def createRingString(numvars):
+    base = '('
+    for x in range(numvars):
+        base = base+(chr(x+65))+','
+    base = base[:-1] + ')'
+    return base
+
 # function that does the heavy lifting of creating polynomials, builds a list of term objects
 
 def createPolynomial(newpoly):
     polynomial = []
-    varNames = createVarNames(newpoly)
     for Y in range(newpoly.t):
-        polynomial.append(term())
-        for X in polynomial:
-        polynomial.append(term(varNames[int(random.uniform(0, newpoly.v))], int(random.uniform(1,newpoly.c)), int(random.uniform(1,newpoly.e))))
+        polynomial.append(term(newpoly.v, int(random.uniform(1,newpoly.c)), newpoly.e))
     return polynomial
 
-# function that converts polynomial lists into usable forms
+# function that converts polynomial lists into strings interpretable by singular (sans errant unary operators)
 
 def fixpoly(polyinlistform):
     stringPoly = ''
+    varNames = createVarNames(polyinlistform[0].v)
     for X in range(len(polyinlistform)):
-        stringPoly = stringPoly+'+'+(str(polyinlistform[X].c)+polyinlistform[X].v+str(polyinlistform[X].e))
+        stringPoly = stringPoly+'+'+str(polyinlistform[X].c)
+        for Y in range(0,polyinlistform[X].f):
+            stringPoly = stringPoly+varNames[int(random.uniform(0, polyinlistform[X].v))]+str(int(random.uniform(1,polyinlistform[X].e)))
     return stringPoly[1:]
 
 # friendly user interface for math nerds
 
-def iI():
+def start():
     print('Hello! Please enter the number of polynomials you would like to test:')
     attempts = int(input())
     print('Please enter the number of terms each polynomial should have:')
     terms = int(input())
     print('Please enter the maximum size of the coefficient on each of the terms:')
     maxcoeff = int(input())
-    print('Please enter the maximum size of the exponent any variable can have inside of a single term: (currently nonfunctional)')
+    print('Please enter the maximum size of the exponent any variable can have inside of a single term:')
     maxexp = int(input())
     print('Please enter the number of variables you would like:')
     Numvars = int(input())
-    return fixpoly(createPolynomial(poly(terms, maxcoeff, maxexp, Numvars)))
+    return test(attempts, terms, maxcoeff, maxexp, Numvars)
 
 # central testing function to clear polynomials pre manual beta invariant screening
 
-def test(attempts, terms, maxcoeff, maxexp):
+def test(attempts, terms, maxcoeff, maxexp,Numvars):
     count = 0
     total = str(attempts)
-    current = singular.ring(0,'(x,y,z)','ds')
+    current = singular.ring(0,createRingString(Numvars),'ds')
     for x in range(attempts):
-        num = 0
         polys = []
-        while num < 2:
-            count = count+1
-            newpoly = createpoly(terms, maxcoeff,maxexp)
-            polynomial = singular(str(createPolynomial(newpoly)))
-            if singular.dim_slocus(polynomial)==2:
-                polys.append(polynomial)
-            if len(polys) == 2:
-                multipliedPoly = polys[0]*polys[1]
+        polynomial = singular(fixpoly(createPolynomial(poly(terms,maxcoeff,maxexp,Numvars))))
+        if singular.dim_slocus(polynomial)==2:
+            polys.append(polynomial)
+        if len(polys) == 2:
+            multipliedPoly = polys[0]*polys[1]
+            print(multipliedPoly)
+            polys = []
+            if singular.dim_slocus(multipliedPoly) ==1:
                 print(multipliedPoly)
-                polys = []
-                if singular.dim_slocus(multipliedPoly) ==1:
-                    print(multipliedPoly)
-                if singular.dim_slocus(multipliedPoly) == 1 and singular.is_is(multipliedPoly.jacob())==0 and len(singular.minAssGTZ(multipliedPoly))==1:
-                    print(multipliedPoly)
-                    count=count+1
-                    num = num+1
+            if singular.dim_slocus(multipliedPoly) == 1 and singular.is_is(multipliedPoly.jacob())==0 and len(singular.minAssGTZ(multipliedPoly))==1:
+                print(multipliedPoly)
+                count=count+1
+                num = num+1
     print(str(count)+" out of "+total+" were successful.")
-    return 0
-
+    return 'complete'
 # no common factors, one dimension singular sets
 
